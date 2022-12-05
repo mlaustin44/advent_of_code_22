6651e2076f102b3f3749ac81b095c11b79b270d4
Matthew Austin <matthew@aescape.co>
2022-12-05 15:36:13 -0800

    solved day 4 part 1

--- 04/cpp/solution.cpp
+++ 04/cpp/solution.cpp
@@ -21,6 +21,8 @@ vector< elfPair> parseInput(string inFile) {
     if (file.is_open()) {
         while (getline(file, line)) {
             elfPair currentPair;
+            // parse the input by splitting on symbols
+            // could be done cleaner with a regex
             auto commaPos = line.find(',');
             auto hyphen1pos = line.find('-');
             auto hyphen2pos = line.find('-', commaPos);
@@ -28,7 +30,6 @@ vector< elfPair> parseInput(string inFile) {
             currentPair.elf1end = stoi(line.substr(hyphen1pos+1, commaPos));
             currentPair.elf2start = stoi(line.substr(commaPos+1,hyphen2pos));
             currentPair.elf2end = stoi(line.substr(hyphen2pos+1, line.size()));
-            cout << currentPair.elf2end << "\n";
             input.push_back(currentPair);
         }
         file.close();
@@ -36,7 +37,24 @@ vector< elfPair> parseInput(string inFile) {
     return input;
 }
 
+int solvePart1(vector<elfPair> input) {
+    int containedCount = 0;
+    for (auto& pair : input) {
+        if ( // case where elf2 is totally contained in elf1
+            (pair.elf2start >= pair.elf1start) && 
+            (pair.elf2end <= pair.elf1end)) {
+                containedCount++;
+        } else if (
+            (pair.elf1start >= pair.elf2start) &&
+            (pair.elf1end <= pair.elf2end)) {
+                containedCount++;
+        }
+    }
+    return containedCount;
+}
+
 int main(int argc, char* argv[]) {
     auto input = parseInput(argv[1]);
-    cout << input.size() << "\n";
+    auto part1soln = solvePart1(input);
+    cout << "Part 1 solution: " << part1soln << "\n";
 }
