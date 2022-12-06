4b91f35ef6ec05479c52a8cbca067fa274329130
Matthew Austin <matthew@aescape.co>
2022-12-06 16:08:10 -0500

    day 6 part 2, moved to a general solution for both parts

--- 06/cpp/solution.cpp
+++ 06/cpp/solution.cpp
@@ -15,18 +15,20 @@ string parseInput(string fName) {
     return line;
 }
 
-int solvePart1(string input) {
-    for (int i=4; i<input.size(); i++) {
+int solve(string input, int windowSize) {
+    for (int i=windowSize; i<input.size(); i++) {
         // a set _only_ contains unique objects
-        set<char> window(input.begin() + i - 4, input.begin() + i);
+        set<char> window(input.begin() + i - windowSize, input.begin() + i);
         // so if the set size is 4, we know all the elements are unique
-        if (window.size() == 4) { return i; }
+        if (window.size() == windowSize) { return i; }
     }
     return -1;
 }
 
 int main(int argc, char* argv[]) {
     string input = parseInput(argv[1]);
-    int part1soln = solvePart1(input);
+    int part1soln = solve(input, 4);
     cout << "Part 1 solution is: " << part1soln << "\n";
+    int part2soln = solve(input, 14);
+    cout << "Part 2 solution is: " << part2soln << "\n";
 }
